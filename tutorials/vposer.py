bca71ea8394d04debfd1f89da1f8e5827c6d4f7a
Jason Chen <chenxin199305@163.com>
2025-09-12 22:30:54 +0800

    refine content

--- tutorials/vposer.py
+++ tutorials/vposer.py
@@ -7,14 +7,14 @@ from body_visualizer.tools.vis_tools import show_image
 
 # ====================================================================================================
 
-# This tutorial requires 'vposer_v2_05'
+# This tutorial requires "vposer_v2_05"
 
 from os import path as osp
 
-support_dir = '../support_data/dowloads'
-expr_dir = osp.join(support_dir, 'vposer_v2_05')  # 'TRAINED_MODEL_DIRECTORY'  in this directory the trained model along with the model code exist
-bm_fname = osp.join(support_dir, 'models/smplx/neutral/model.npz')  # 'PATH_TO_SMPLX_model.npz'  obtain from https://smpl-x.is.tue.mpg.de/downloads
-sample_amass_fname = osp.join(support_dir, 'amass_sample.npz')  # a sample npz file from AMASS
+support_dir = "../support_data/dowloads"
+expr_dir = osp.join(support_dir, "vposer_v2_05")  # "TRAINED_MODEL_DIRECTORY"  in this directory the trained model along with the model code exist
+bm_fname = osp.join(support_dir, "models/smplx/neutral/model.npz")  # "PATH_TO_SMPLX_model.npz"  obtain from https://smpl-x.is.tue.mpg.de/downloads
+sample_amass_fname = osp.join(support_dir, "amass_sample.npz")  # a sample npz file from AMASS
 
 print(
     f"expr_dir = {expr_dir}\n"
@@ -27,7 +27,7 @@ print(
 # Loading SMPLx Body Model
 from human_body_prior.body_model.body_model import BodyModel
 
-bm = BodyModel(bm_fname=bm_fname).to('cuda')
+bm = BodyModel(bm_fname=bm_fname).to("cuda")
 
 # ====================================================================================================
 
@@ -37,15 +37,15 @@ from human_body_prior.models.vposer_model import VPoser
 
 vp, ps = load_model(expr_dir,
                     model_code=VPoser,
-                    remove_words_in_model_weights='vp_model.',
+                    remove_words_in_model_weights="vp_model.",
                     disable_grad=True)
-vp = vp.to('cuda')
+vp = vp.to("cuda")
 
 # ====================================================================================================
 
 # Prepare the pose_body from amass sample
-amass_body_pose = np.load(sample_amass_fname)['poses'][:, 3:66]
-amass_body_pose = torch.from_numpy(amass_body_pose).type(torch.float).to('cuda')
+amass_body_pose = np.load(sample_amass_fname)["poses"][:, 3:66]
+amass_body_pose = torch.from_numpy(amass_body_pose).type(torch.float).to("cuda")
 
 print(
     f"amass_body_pose.shape = {amass_body_pose.shape}"
@@ -59,13 +59,13 @@ print(
 
 # ====================================================================================================
 
-amass_body_pose_rec = vp.decode(amass_body_poZ)['pose_body'].contiguous().view(-1, 63)
+amass_body_pose_rec = vp.decode(amass_body_poZ)["pose_body"].contiguous().view(-1, 63)
 
 print(
     f"amass_body_pose_rec.shape = {amass_body_pose_rec.shape}"
 )
 
-# Let's visualize the original pose and the reconstructed one:
+# Let"s visualize the original pose and the reconstructed one:
 
 t = np.random.choice(len(amass_body_pose))
 
@@ -75,6 +75,6 @@ print(
     f"all_pose_body.shape = {all_pose_body.shape}"
 )
 
-images = render_smpl_params(bm, {'pose_body': all_pose_body}).reshape(1, 2, 1, 400, 400, 3)
+images = render_smpl_params(bm, {"pose_body": all_pose_body}).reshape(1, 2, 1, 400, 400, 3)
 img = imagearray2file(images)
 show_image(img[0])
--- tutorials/vposer_sampling.py
+++ tutorials/vposer_sampling.py
@@ -6,13 +6,13 @@ from body_visualizer.tools.vis_tools import imagearray2file, show_image
 
 # ====================================================================================================
 
-# This tutorial requires 'vposer_v2_05'
+# This tutorial requires "vposer_v2_05"
 
 from os import path as osp
 
-support_dir = '../support_data/dowloads'
-expr_dir = osp.join(support_dir, 'vposer_v2_05')  # 'TRAINED_MODEL_DIRECTORY'  in this directory the trained model along with the model code exist
-bm_fname = osp.join(support_dir, 'models/smplx/neutral/model.npz')  # 'PATH_TO_SMPLX_model.npz'  obtain from https://smpl-x.is.tue.mpg.de/downloads
+support_dir = "../support_data/dowloads"
+expr_dir = osp.join(support_dir, "vposer_v2_05")  # "TRAINED_MODEL_DIRECTORY"  in this directory the trained model along with the model code exist
+bm_fname = osp.join(support_dir, "models/smplx/neutral/model.npz")  # "PATH_TO_SMPLX_model.npz"  obtain from https://smpl-x.is.tue.mpg.de/downloads
 
 print(
     f"expr_dir = {expr_dir}\n"
@@ -24,7 +24,7 @@ print(
 # Loading SMPLx Body Model
 from human_body_prior.body_model.body_model import BodyModel
 
-bm = BodyModel(bm_fname=bm_fname).to('cuda')
+bm = BodyModel(bm_fname=bm_fname).to("cuda")
 
 # ====================================================================================================
 
@@ -34,9 +34,9 @@ from human_body_prior.models.vposer_model import VPoser
 
 vp, ps = load_model(expr_dir,
                     model_code=VPoser,
-                    remove_words_in_model_weights='vp_model.',
+                    remove_words_in_model_weights="vp_model.",
                     disable_grad=True)
-vp = vp.to('cuda')
+vp = vp.to("cuda")
 
 # ====================================================================================================
 
@@ -44,8 +44,8 @@ vp = vp.to('cuda')
 num_poses = 9
 
 # will a generate Nx1x21x3 tensor of body poses
-sampled_pose_body = vp.sample_poses(num_poses=num_poses)['pose_body'].contiguous().view(num_poses, -1)
-images = render_smpl_params(bm, {'pose_body': sampled_pose_body}).reshape(3, 3, 1, 400, 400, 3)
+sampled_pose_body = vp.sample_poses(num_poses=num_poses)["pose_body"].contiguous().view(num_poses, -1)
+images = render_smpl_params(bm, {"pose_body": sampled_pose_body}).reshape(3, 3, 1, 400, 400, 3)
 img = imagearray2file(images)
 show_image(np.array(img[0]))
 
